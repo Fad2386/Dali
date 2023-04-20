0caf1b47df8699ed3ab75c51386067499c0a4835
Dali <surrealizm.2012@gmail.com>
2023-04-20 23:51:30 +0200

    test

---
+++ Script.js
@@ -0,0 +1,27 @@
+// alert("Hello World")
+// console.log("Hello")
+// prompt("Как вас зовут?")
+// const username = prompt("Как вас зовут?")
+// console.log(username)
+
+// console.log("Hello")
+// console.log("Меня зовут Dali")
+// console.log("мне 12 лет")
+
+// alert("Выберитие какой-то из вариантов")
+// alert("Заполните все поля формы")
+// const userage = prompt("Сколько Вам лет?")
+// console.log(userage)
+
+// const userName = prompt("Как вас зовут?")
+// const userage = prompt("Cколько вам лет?")
+// console.log("Привет, " + userName + " вам " + userage + " лет")
+// console.log(`Hello, ${userName}, you are ${userage} old.`)
+// const userAge = prompt("Сколько вам годиков?")
+// console.log(2023 - userAge)
+
+const r = 154
+const g = 172
+const b = 65
+
+console.log(` rgb ${r}, ${g}, ${b}`)
