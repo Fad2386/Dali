3830d6b29d7fbc84c21e65cc0d0b687ea46e011b
Dali <surrealizm.2012@gmail.com>
2023-05-04 22:51:45 +0200

    mew

--- JSHW/JSHW/HW020523/JS.js
+++ JSHW/JSHW/HW020523/JS.js
@@ -1,6 +1,6 @@
 const number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-for (let i = 1; i <= number.length; i++) {
-  if (number[i] % 2 == 1) console.log(number[i]);
+for (let i = 0; i <= 10; i++) {
+  if (i % 2 === 0) console.log(i);
 }
 
 function arrayNumbers(num1, num2) {
