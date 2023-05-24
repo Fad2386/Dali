cd22890300faa939fd34bd339bfb6d5809391485
Dali <surrealizm.2012@gmail.com>
2023-05-25 01:28:35 +0200

    Box

---
+++ JSHW/Java/JavaBasic1/src/main/java/JavaHW1/Box/P.java
@@ -0,0 +1,9 @@
+package JavaHW1.Box;
+
+public class P {
+    public static void main(String[] args) {
+
+        Box box = new Box(2, 3, 4);
+        System.out.println(box.height + box.length + box.width);
+    }
+}
